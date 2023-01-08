c4b86a7fbf933ce640dca76300461b6fc6e063d4
+ <+>
2023-01-08 16:46:40 -0500

    commit tree 2d063761a4632557a2c55abcc8fca7330f3bc858

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let newHeader;
+
+newHeader = document.createElement('h1');
+newHeader.textContent = 'YOUR-NAME is the champion';
+newHeader.textContent.replace('YOUR-NAME', 'Shah');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id', 'victory');
